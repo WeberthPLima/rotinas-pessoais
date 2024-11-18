52c3d0f354649857ac1cadeb0c3c6e674f15da78
weberth.lima <weberth.lima@intra.local>
2024-11-18 01:12:31 -0300

    reativando pontos

--- app/Controllers/CronsJobs/baterPontoDecisaoNODATA.ts
+++ app/Controllers/CronsJobs/baterPontoDecisaoNODATA.ts
@@ -14,40 +14,40 @@ export default class BaterPontoNODATAController {
 
     console.log(`${dataFormatada}T${hora}:${minuto}:${random}-03:00`)
 
-    // const url = 'https://pontogo-api.herokuapp.com/add-point?company-token-pg=pvMFZKLoI9CpkjHCcm6y&employee-token-pg=97Z4hhrMARnCX8w7QIzB';
-    // const body = {
-    //   date: `${dataFormatada}T${hora}:${minuto}:${random}-03:00`,
-    //   latitude: -15.6593449,
-    //   longitude: -48.1956026,
-    //   userData: {
-    //     device: "Desktop",
-    //     gatewayMac: null,
-    //     ip: "",
-    //     operatingSystem: "Mac OS 10"
-    //   }
-    // };
+    const url = 'https://pontogo-api.herokuapp.com/add-point?company-token-pg=pvMFZKLoI9CpkjHCcm6y&employee-token-pg=97Z4hhrMARnCX8w7QIzB';
+    const body = {
+      date: `${dataFormatada}T${hora}:${minuto}:${random}-03:00`,
+      latitude: -15.6593449,
+      longitude: -48.1956026,
+      userData: {
+        device: "Desktop",
+        gatewayMac: null,
+        ip: "",
+        operatingSystem: "Mac OS 10"
+      }
+    };
 
     try {
-      // const response = await fetch(url, {
-      //   method: 'POST',
-      //   headers: {
-      //     'Accept': '*/*',
-      //     'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6',
-      //     'Connection': 'keep-alive',
-      //     'Content-Type': 'application/json',
-      //     'Origin': 'https://beta.pontogo.app',
-      //     'Referer': 'https://beta.pontogo.app/',
-      //     'Authorization': 'B514A58B-E7BE-42D6-7205-9645FD3CBC7C',
-      //     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
-      //   },
-      //   body: JSON.stringify(body),
-      // });
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': '*/*',
+          'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6',
+          'Connection': 'keep-alive',
+          'Content-Type': 'application/json',
+          'Origin': 'https://beta.pontogo.app',
+          'Referer': 'https://beta.pontogo.app/',
+          'Authorization': 'B514A58B-E7BE-42D6-7205-9645FD3CBC7C',
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+        },
+        body: JSON.stringify(body),
+      });
 
-      // const data = await response.json();
+      const data = await response.json();
       console.log('Ponto registrado:', data);
     } catch (error) {
       console.error('Erro ao registrar o ponto:', error);
-      // console.error('Body error:', body);
+      console.error('Body error:', body);
     }
   }
 }
