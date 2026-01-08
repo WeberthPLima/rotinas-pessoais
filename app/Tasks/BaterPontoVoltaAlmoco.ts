cbdf644fa540b0baecbddffa8c448d3b57b853cf
weberth.lima <weberth.lima@intra.local>
2026-01-07 22:39:05 -0300

    ajustes horarios

--- app/Tasks/BaterPontoVoltaAlmoco.ts
+++ app/Tasks/BaterPontoVoltaAlmoco.ts
@@ -10,7 +10,7 @@ export default class BaterPontoVoltaAlmoco extends BaseTask {
   private static generatedDate: string | null = null
 
   public static get schedule() {
-    return '01-29 16 * * 1-5' // Roda de 13:01 até 13:17
+    return '15-45 16 * * 1-5' // Roda de 13:01 até 13:17
   }
 
   public static get useLock() {
@@ -29,7 +29,7 @@ export default class BaterPontoVoltaAlmoco extends BaseTask {
 
     // Verifica se já foi gerado para hoje
     if (BaterPontoVoltaAlmoco.generatedDate !== todayKey) {
-      BaterPontoVoltaAlmoco.randomMinute = getRandomInt(1, 29)
+      BaterPontoVoltaAlmoco.randomMinute = getRandomInt(15, 45)
       BaterPontoVoltaAlmoco.generatedDate = todayKey
       console.log(`[13] 🎲 Novo minuto aleatório do dia: ${BaterPontoVoltaAlmoco.randomMinute}`)
     }
