3b63e800f066752534df713eef5f4a74a74443df
weberth.lima <weberth.lima@intra.local>
2026-09-17 09:02:33 -0300

    ajuste: alterando intervalo de minutos aleatórios para o ponto de volta do almoço

--- app/Tasks/BaterPontoVoltaAlmoco.ts
+++ app/Tasks/BaterPontoVoltaAlmoco.ts
@@ -10,7 +10,7 @@ export default class BaterPontoVoltaAlmoco extends BaseTask {
   private static generatedDate: string | null = null
 
   public static get schedule() {
-    return '15-48 16 * * 1-5' // Roda de 13:01 até 13:17
+    return '35-59 16 * * 1-5' // Roda de 13:01 até 13:17
   }
 
   public static get useLock() {
@@ -29,7 +29,7 @@ export default class BaterPontoVoltaAlmoco extends BaseTask {
 
     // Verifica se já foi gerado para hoje
     if (BaterPontoVoltaAlmoco.generatedDate !== todayKey) {
-      BaterPontoVoltaAlmoco.randomMinute = getRandomInt(15, 48)
+      BaterPontoVoltaAlmoco.randomMinute = getRandomInt(35, 59)
       BaterPontoVoltaAlmoco.generatedDate = todayKey
       console.log(`[13] 🎲 Novo minuto aleatório do dia: ${BaterPontoVoltaAlmoco.randomMinute}`)
     }
